4d05e4e2427a5a1a629963559bc17325e55aa146
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-07-29 06:49:39 +0300

    add pytest

---
+++ tests/test_html2text_rs.py
@@ -0,0 +1,21 @@
+import pytest
+
+import html2text_rs
+
+
+def test_text_markdown():
+    html = "<h1>Hello World</h1><p>This is a test.</p>"
+    expected_output = "# Hello World\n\nThis is a test.\n"
+    result = html2text_rs.text_markdown(html, width=80)
+    assert (
+        result == expected_output
+    ), f"\nExpected:\n {expected_output} \nGot:\n {result}"
+
+
+def test_text_plain():
+    html = "<h1>Hello World</h1><p>This is a test.</p>"
+    expected_output = "Hello World\n\nThis is a test.\n"
+    result = html2text_rs.text_plain(html, width=80)
+    assert (
+        result == expected_output
+    ), f"\nExpected:\n {expected_output} \nGot:\n {result}"
