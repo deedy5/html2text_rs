48c6359cc792d9172158fa924b5651353776a090
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-03-18 09:57:47 +0300

    perf: avoid redundant html copying

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,50 @@ use html2text::{
     from_read, from_read_with_decorator,
     render::{RichDecorator, TrivialDecorator},
 };
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyString};
 
 /// Convert HTML to markdown text
+#[inline(always)]
 #[pyfunction]
 #[pyo3(signature=(html, width=100))]
-fn text_markdown(html: String, width: usize, py: Python) -> PyResult<String> {
+fn text_markdown<'py>(html: &Bound<'py, PyString>, width: usize) -> PyResult<String> {
+    let py = html.py();
+    let html_string = html.clone().unbind();
+    let html_cow = html_string.to_cow(py)?;
     let text = py
-        .allow_threads(|| from_read(html.as_bytes(), width))
+        .allow_threads(|| from_read(html_cow.as_bytes(), width))
         .expect("Error when extracting markdown text");
     Ok(text)
 }
 
 /// Convert HTML to plain text
+#[inline(always)]
 #[pyfunction]
 #[pyo3(signature=(html, width=100))]
-fn text_plain(html: String, width: usize, py: Python) -> PyResult<String> {
+fn text_plain<'py>(html: &Bound<'py, PyString>, width: usize) -> PyResult<String> {
+    let py = html.py();
+    let html_string = html.clone().unbind();
+    let html_cow = html_string.to_cow(py)?;
     let text = py
-        .allow_threads(|| from_read_with_decorator(html.as_bytes(), width, TrivialDecorator::new()))
+        .allow_threads(|| {
+            from_read_with_decorator(html_cow.as_bytes(), width, TrivialDecorator::new())
+        })
         .expect("Error when extracting plain text");
     Ok(text)
 }
 
 /// Convert HTML to rich text
+#[inline(always)]
 #[pyfunction]
 #[pyo3(signature=(html, width=100))]
-fn text_rich(html: String, width: usize, py: Python) -> PyResult<String> {
+fn text_rich<'py>(html: &Bound<'py, PyString>, width: usize) -> PyResult<String> {
+    let py = html.py();
+    let html_string = html.clone().unbind();
+    let html_cow = html_string.to_cow(py)?;
     let text = py
-        .allow_threads(|| from_read_with_decorator(html.as_bytes(), width, RichDecorator::new()))
+        .allow_threads(|| {
+            from_read_with_decorator(html_cow.as_bytes(), width, RichDecorator::new())
+        })
         .expect("Error when extracting rich text");
     Ok(text)
 }
