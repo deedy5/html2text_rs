206d680fc66f4db4d111aae38945648237c629d7
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-08-30 19:18:05 +0300

    refactor: use detach to release the GIL

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ fn text_markdown<'py>(html: &Bound<'py, PyString>, width: usize) -> PyResult<Str
     let html_string = html.clone().unbind();
     let html_cow = html_string.to_cow(py)?;
     let text = py
-        .allow_threads(|| from_read(html_cow.as_bytes(), width))
+        .detach(|| from_read(html_cow.as_bytes(), width))
         .expect("Error when extracting markdown text");
     Ok(text)
 }
@@ -27,9 +27,7 @@ fn text_plain<'py>(html: &Bound<'py, PyString>, width: usize) -> PyResult<String
     let html_string = html.clone().unbind();
     let html_cow = html_string.to_cow(py)?;
     let text = py
-        .allow_threads(|| {
-            from_read_with_decorator(html_cow.as_bytes(), width, TrivialDecorator::new())
-        })
+        .detach(|| from_read_with_decorator(html_cow.as_bytes(), width, TrivialDecorator::new()))
         .expect("Error when extracting plain text");
     Ok(text)
 }
@@ -43,9 +41,7 @@ fn text_rich<'py>(html: &Bound<'py, PyString>, width: usize) -> PyResult<String>
     let html_string = html.clone().unbind();
     let html_cow = html_string.to_cow(py)?;
     let text = py
-        .allow_threads(|| {
-            from_read_with_decorator(html_cow.as_bytes(), width, RichDecorator::new())
-        })
+        .detach(|| from_read_with_decorator(html_cow.as_bytes(), width, RichDecorator::new()))
         .expect("Error when extracting rich text");
     Ok(text)
 }
